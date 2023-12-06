30e341ef726c1a8aa1a5f2e072b70c88a3944323
bobcamps065 <brunocamps1@hotmail.com>
2023-12-06 20:59:53 -0300

    first commit

---
+++ nessletter.php
@@ -0,0 +1,43 @@
+<?php 
+
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $dba = "newsletter";
+
+// $email = $_POST['email'];
+
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// $mysqli = new mysqli('localhost', 'root', '','newsletter');
+
+// if($mysqli->connect_error) {
+
+//     die('Erro no banco de dados: ' . $mysqli->connect_error);
+
+// } else {
+    
+//     echo  "Connection Successful!" . "<br>";
+//     echo "<hr>";
+
+// }
+
+// $stmt = $mysqli->prepare("INSERT INTO noticias (newsletter) VALUES (?)");
+// $stmt->bind_param('s', $email);
+// $stmt->execute();
+
+// if (!empty($_POST['email']) == false) {
+    
+//     die('Erro no envio');
+// } else{
+//     echo "Envio com sucesso!";
+// }
+
+
+// $stmt->close();
+// $mysqli->close();
+
+$name = array("Bruno", "Lidia", "Lucca");
+
+
+
+?>
